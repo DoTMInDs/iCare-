4b4900392beec5494c7d78c39b8e5ce5a3c3bc9d
esmond <dhonniemccony3@gmail.com>
2026-05-16 10:33:37 -1200

    updated build for creating user

--- build.sh
+++ build.sh
@@ -9,9 +9,9 @@ python manage.py collectstatic --no-input
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput || python manage.py migrate webpush --fake
 
-if [ "$DJANGO_CREATEUSER" == "1" ]; then 
-    python manage.py createsuperuser --noinput
-fi
+# if [ "$DJANGO_CREATEUSER" == "1" ]; then 
+#     python manage.py createsuperuser --noinput
+# fi
 
 # python yedulo/manage.py runserver 0.0.0.0:$PORT
 # python -m gunicorn yedulo/yedulo.asgi:application -k uvicorn.workers.UvicornWorker
